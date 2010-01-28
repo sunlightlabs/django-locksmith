a3dd4c26f72d3ffc3f94789a468a301f2fc5b1f2
James Turk <james.p.turk@gmail.com>
2010-01-28 15:15:45 -0500

    working pushkeys

--- locksmith/auth/management/commands/pushkeys.py
+++ locksmith/auth/management/commands/pushkeys.py
@@ -9,13 +9,16 @@ class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
         dirty_keys = Key.objects.exclude(pub_status=PUBLISHED)
-        endpoints = {UNPUBLISHED: urljoin(settings.LOCKSMITH_HUB_URL, '/publish_key/'),
-                     NEEDS_UPDATE: urljoin(settings.LOCKSMITH_HUB_URL, '/update_key/')}
-        for key in dirty_keys:
+        endpoints = {UNPUBLISHED: urljoin(settings.LOCKSMITH_HUB_URL, 'create_key/'),
+                     NEEDS_UPDATE: urljoin(settings.LOCKSMITH_HUB_URL, 'update_key/')}
+        actions = {UNPUBLISHED: 0, NEEDS_UPDATE: 0}
 
+        for key in dirty_keys:
             apicall(endpoints[key.pub_status], settings.LOCKSMITH_SIGNING_KEY,
                     api=settings.LOCKSMITH_API_NAME, key=key.key,
                     email=key.email, status=key.status)
-
+            actions[key.pub_status] += 1
             key.pub_status = PUBLISHED
             key.save()
+
+        print 'Published %s new keys, updated %s keys.' % (actions[UNPUBLISHED], actions[NEEDS_UPDATE])
