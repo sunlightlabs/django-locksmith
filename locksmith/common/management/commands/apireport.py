98b63e080fc84a83f6630e25858afd7c83e555c5
James Turk <james.p.turk@gmail.com>
2010-01-28 21:10:02 -0500

    LOCKSMITH_STATS_ settings

--- locksmith/common/management/commands/apireport.py
+++ locksmith/common/management/commands/apireport.py
@@ -5,40 +5,38 @@ from django.conf import settings
 from django.db.models import get_model, Count
 from locksmith.common import apicall
 
-APISTATS_APP = 'api'
-APISTATS_MODEL = 'LogEntry'
-APISTATS_DATE_FIELD = 'timestamp'
-APISTATS_ENDPOINT_FIELD = 'method'
-APISTATS_USER_FIELD = 'caller_key'
+APP = getattr(settings, 'LOCKSMITH_STATS_APP', 'api')
+MODEL = getattr(settings, 'LOCKSMITH_STATS_MODEL', 'LogEntry')
+DATE_FIELD = getattr(settings, 'LOCKSMITH_STATS_DATE_FIELD', 'timestamp')
+ENDPOINT_FIELD = getattr(settings, 'LOCKSMITH_STATS_ENDPOINT_FIELD', 'method')
+USER_FIELD = getattr(settings, 'LOCKSMITH_STATS_USER_FIELD', 'caller_key')
+
+LogModel = get_model(APP, MODEL)
 
 class Command(BaseCommand):
     help = "Push a given day's logs up to the analytics hub"
     args = '[date]'
-
     requires_model_validation = False
 
     def handle(self, date=None, *args, **options):
+        # calculate begin & end
         if date:
             begin = datetime.datetime.strptime(date, '%Y-%m-%d')
         else:
             begin = datetime.datetime.now() - datetime.timedelta(days=1)
             date = begin.strftime('%Y-%m-%d')
-
         end = begin + datetime.timedelta(days=1)
 
-        timestamp_fieldname = '%s__range' % APISTATS_DATE_FIELD
+        # construct database query
+        timestamp_fieldname = '%s__range' % DATE_FIELD
+        qs = LogModel.objects.filter(**{timestamp_fieldname : (begin, end)})
+        results = qs.values(ENDPOINT_FIELD, USER_FIELD).annotate(calls=Count('id'))
 
-        Model = get_model(APISTATS_APP, APISTATS_MODEL)
-        qs = Model.objects.filter(**{timestamp_fieldname : (begin, end)})
-        results = qs.values(APISTATS_ENDPOINT_FIELD, APISTATS_USER_FIELD).annotate(calls=Count('id'))
+        endpoint = urljoin(settings.LOCKSMITH_HUB_URL, 'report_views/')
 
-        endpoint = urljoin(settings.LOCKSMITH_HUB_URL, '/report_views/')
-
+        # report results
         for item in results:
-            data = {'api': settings.LOCKSMITH_API_NAME, 'date':date,
-                    'endpoint': item[APISTATS_ENDPOINT_FIELD],
-                    'key': item[APISTATS_USER_FIELD], 'calls': item['calls']}
             apicall(endpoint, settings.LOCKSMITH_SIGNING_KEY,
                     api=settings.LOCKSMITH_API_NAME, date=date,
-                    endpoint=item[APISTATS_ENDPOINT_FIELD],
-                    key=item[APISTATS_USER_FIELD], calls=item['calls'])
+                    endpoint=item[ENDPOINT_FIELD],
+                    key=item[USER_FIELD], calls=item['calls'])
