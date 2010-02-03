55eb796f5e7e0b6012fbd8345e2a03c75a9786c1
James Turk <james.p.turk@gmail.com>
2010-02-03 13:13:25 -0500

    updates to setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@
 from distutils.core import setup
 
 setup(name='django-locksmith',
-      version='0.1.0',
+      version='0.2.0',
       description='Django apps for API authentication and centralized authorization',
       author='James Turk',
       author_email='jturk@sunlightfoundation.com',
       license='BSD',
       url='http://github.com/sunlightlabs/django-locksmith/',
       packages=['locksmith', 'locksmith.auth', 'locksmith.auth.management',
-                'locksmith.auth.management.commands', 'locksmith.common',
-                'locksmith.common.management',
-                'locksmith.common.management.commands', 'locksmith.hub']
+                'locksmith.auth.management.commands', 'locksmith.hub',
+                'locksmith.hub.management', 'locksmith.hub.management.commands']
 )
