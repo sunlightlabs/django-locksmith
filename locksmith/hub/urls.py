6ddac2c939568818feb02fc9fca12f2d94224899
James Turk <james.p.turk@gmail.com>
2010-02-03 11:45:40 -0500

    split urls into analytics and accounts

--- locksmith/hub/urls.py
+++ locksmith/hub/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('locksmith.hub.views',
-    url(r'^report_calls/$', 'report_calls', name='report_calls'),
+accounts = patterns('locksmith.hub.views',
     url(r'^register/$', 'register', name='api_registration'),
     url(r'^confirmkey/(?P<key>[0-9a-f]{32})/$', 'confirm_registration',
         name='api_confirm'),
-    url(r'^analytics/$', 'analytics_index', name='analytics_index'),
 )
 
+analytics = patterns('locksmith.hub.views',
+    url(r'^report_calls/$', 'report_calls', name='report_calls'),
+    url(r'^$', 'analytics_index', name='analytics_index'),
+)
+
+urlpatterns = patterns('locksmith.hub.views',
+    url(r'^accounts/', include(accounts)),
+    url(r'^analytics/', include(analytics)),
+)
