2d34a073932cc61a469e0c9e86be44465e3fa67f
James Turk <james.p.turk@gmail.com>
2010-02-03 14:08:14 -0500

    fix auth.urls

--- locksmith/auth/urls.py
+++ locksmith/auth/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('locksmith.auth.views',
-    url(r'^create_key/$', self.create_key_view, name='create_key'),
-    url(r'^update_key/$', self.update_key_view, name='update_key'),
-    url(r'^update_key_by_email/$', self.update_key_view, {'get_by':'email'},
+    url(r'^create_key/$', 'create_key', name='create_key'),
+    url(r'^update_key/$', 'update_key', name='update_key'),
+    url(r'^update_key_by_email/$', 'update_key', {'get_by':'email'},
         name='update_key_by_email'),
 )
 
