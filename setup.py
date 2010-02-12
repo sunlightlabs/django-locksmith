124324325b2989b8133d22250765415d90064ffa
James Turk <james.p.turk@gmail.com>
2010-02-12 17:13:06 -0500

    include data files in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='django-locksmith',
-      version='0.2.0',
+      version='0.3.0',
       description='Django apps for API authentication and centralized authorization',
       author='James Turk',
       author_email='jturk@sunlightfoundation.com',
@@ -11,5 +11,8 @@ setup(name='django-locksmith',
       url='http://github.com/sunlightlabs/django-locksmith/',
       packages=['locksmith', 'locksmith.auth', 'locksmith.auth.management',
                 'locksmith.auth.management.commands', 'locksmith.hub',
-                'locksmith.hub.management', 'locksmith.hub.management.commands']
+                'locksmith.hub.management', 'locksmith.hub.management.commands',
+                'locksmith.hub.templatetags'],
+      package_data={'locksmith': ['media/scripts/*.js'],
+                    'locksmith.hub': ['templates/locksmith/*.html']},
 )
