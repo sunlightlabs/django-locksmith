cbb7106441791403c61c6785d78ddf7a58775125
James Turk <james.p.turk@gmail.com>
2010-03-24 13:08:53 -0400

    better auth_backend

--- locksmith/hub/auth_backend.py
+++ locksmith/hub/auth_backend.py
@@ -4,28 +4,27 @@ from locksmith.hub.models import Key
 class LocksmithBackend:
     """
     Authenticate with email address/key.
+
+    * Try to log in with email+password
+    * If no user exists try and create one with key as password
     """
 
     def authenticate(self, username=None, password=None):
-        # figure out if authenticate works
         try:
-            key = Key.objects.get(email=username)
+            # if user exists with this email try and log in
+            user = User.objects.get(email=username)
+            if user.check_password(username, password):
+                return user
 
-            if key.key == password:
-                user, created = User.objects.get_or_create(email=username,
-                                   defaults={'username':username[:30]})
-                if created:
-                    user.set_password(password)
-                    return user
-            else:
-                try:
-                    user = User.objects.get(email=username)
-                    if user.check_password(password):
-                        return user
-                except User.DoesNotExist:
-                    return None
-        except Key.DoesNotExist:
-            return None
+        except User.DoesNotExist:
+            # if no user exists try and create one based on the key
+            try:
+                key = Key.objects.get(email=username)
+                if key.key == password:
+                    return User.objects.create_user(username[:30], username,
+                                                    password)
+            except Key.DoesNotExist:
+                return None
 
 
     def get_user(self, user_id):
