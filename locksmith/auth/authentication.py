452eb8e7def7c3d7a079366a64da587406790120
James Turk <james.p.turk@gmail.com>
2010-05-03 15:37:19 -0400

    remove WWW-Authenticate header

--- locksmith/auth/authentication.py
+++ locksmith/auth/authentication.py
@@ -2,12 +2,11 @@ from django.conf import settings
 from django.http import HttpResponse
 
 class PistonKeyAuthentication(object):
-    
+
     def is_authenticated(self, request):
         return hasattr(request, 'apikey') and request.apikey.status == 'A'
-    
+
     def challenge(self):
         resp = HttpResponse("Authorization Required", content_type="text/plain")
-        resp['WWW-Authenticate'] = 'Basic realm="API"'
         resp.status_code = 401
         return resp
