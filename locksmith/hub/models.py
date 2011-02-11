830b1dda901c3092805a00afc081d7038a1dddfa
James Turk <james.p.turk@gmail.com>
2011-02-11 10:40:46 -0500

    change org_url to a char field

--- locksmith/hub/models.py
+++ locksmith/hub/models.py
@@ -35,7 +35,7 @@ class Key(models.Model):
     status = models.CharField(max_length=1, choices=KEY_STATUSES, default='U')
 
     org_name = models.CharField('Organization Name', max_length=100, blank=True)
-    org_url = models.URLField('Organization URL', blank=True)
+    org_url = models.CharField('Organization URL', blank=True, max_length=200)
     usage = models.TextField('Intended Usage', blank=True)
 
     issued_on = models.DateTimeField(default=datetime.datetime.now, editable=False)
