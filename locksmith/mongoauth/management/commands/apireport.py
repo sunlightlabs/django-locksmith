e2286a577c2c4c44a3a999d518bbc8134a1e0c91
James Turk <james.p.turk@gmail.com>
2011-03-01 11:48:01 -0500

    remove LogModel references

--- locksmith/mongoauth/management/commands/apireport.py
+++ locksmith/mongoauth/management/commands/apireport.py
@@ -21,9 +21,6 @@ class Command(BaseCommand):
         dt_end = dt_begin + datetime.timedelta(days=1)
 
         # construct database query
-        qs = LogModel.objects.extra(where=["date_trunc('day', {0}) = '{1}'".format(DATE_FIELD, date)]).order_by()
-        results = qs.values(ENDPOINT_FIELD, USER_FIELD).annotate(calls=Count('id'))
-
         results = db.logs.group(['key', 'method'],
                                {"timestamp": {"$gte": dt_begin, "$lt": dt_end}},
                                {"count": 0},
