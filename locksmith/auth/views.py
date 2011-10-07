8d874d2fb2446471c3dd4db8e17407a9c160397c
James Turk <james.p.turk@gmail.com>
2011-10-07 10:45:19 -0400

    CSRF exempt views

--- locksmith/auth/views.py
+++ locksmith/auth/views.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 from locksmith.common import get_signature
 from locksmith.auth.models import ApiKey
 
 def verify_signature(post):
     return get_signature(post, settings.LOCKSMITH_SIGNING_KEY) == post['signature']
 
+@csrf_exempt
 @require_POST
 def create_key(request):
     if not verify_signature(request.POST):
@@ -17,6 +19,8 @@ def create_key(request):
                           status=request.POST['status'])
     return HttpResponse('OK')
 
+
+@csrf_exempt
 @require_POST
 def update_key(request, get_by='key'):
     if not verify_signature(request.POST):
