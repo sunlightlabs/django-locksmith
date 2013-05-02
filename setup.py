eb93ee59cea3616578267e8aff0b1493fc923cac
Drew Vogel <dvogel@sunlightfoundation.com>
2013-05-02 17:25:59 -0400

    v0.7.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-locksmith',
-      version='0.7.2',
+      version='0.7.3',
       description='Django apps for API authentication and centralized authorization',
       author='James Turk',
       author_email='jturk@sunlightfoundation.com',
