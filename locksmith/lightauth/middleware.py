c027ffc6cbe742658bd271899a6d363c4a8c7fe7
Andrew Pendleton <apendleton@sunlightfoundation.com>
2013-12-18 12:46:31 -0500

    Cache API key check response.

--- locksmith/lightauth/middleware.py
+++ locksmith/lightauth/middleware.py
@@ -1,5 +1,8 @@
 from django.conf import settings
-from locksmith.lightauth.common import check_key
+from locksmith.lightauth.common import check_key as _check_key
+from locksmith.common import cache
+
+check_key = cache(seconds=86400)(_check_key)
 
 QS_PARAM = getattr(settings, 'LOCKSMITH_QS_PARAM', 'apikey')
 HTTP_HEADER = getattr(settings, 'LOCKSMITH_HTTP_HEADER', 'HTTP_X_APIKEY')
