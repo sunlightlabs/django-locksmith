293492503aa438aa7e016af336634b09d18f3e9d
Jeremy Carbaugh <jcarbaugh@gmail.com>
2014-04-08 17:28:42 -0400

    change django.conf.urls.defaults to django.conf.urls

--- locksmith/auth/urls.py
+++ locksmith/auth/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('locksmith.auth.views',
     url(r'^create_key/$', 'create_key', name='create_key'),
